a099cda4c862bc9b4478487ff5fd6ea27771ad47
turgaysozen <turgaysozen@hotmail.com>
2019-12-21 11:21:29 +0300

    react learning projects

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ProductList from "./productsList";
+import Product from "../src/Components/Product";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const productComponent = ProductList.map(item => <Product key={item.id} product={item}/>)
+    return (
+      <div>
+          {productComponent}
+      </div>
+    )
 }
 
 export default App;
---
+++ src/productsList.js
@@ -0,0 +1,16 @@
+const products = [
+  {
+    id : "1",
+    title: 'This is product 1',
+    price: 10,
+    quantity: 10,
+  },
+  {
+    id : "2",
+    title: 'This is product 2',
+    price: 20,
+    quantity: 20,
+  }
+]
+
+export default products;

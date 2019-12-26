e49ea6c5462e34c61402e8fcd2cf2c0db32ef377
turgaysozen <turgaysozen@hotmail.com>
2019-12-26 20:49:10 +0300

    Learning React - Meme Generator

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from "react";
 import "../src/index.css"
 import Header from "../src/Components/Header"
 import MemeGenerator from "../src/Components/MemeGenerator"
-// class App extends Component {
-
-// }
 
 function App() {
     return(
